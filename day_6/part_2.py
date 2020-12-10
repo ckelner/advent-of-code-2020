9fbfb1844e413f2764e010676da489893dc2c32b
ckelner <chris.kelner@gmail.com>
2020-12-10 12:59:19 -0800

    group and empty newlines

--- day_6/part_2.py
+++ day_6/part_2.py
@@ -3,12 +3,12 @@ tmp_answers = ""
 #with open('./debug_input') as fp:
 with open('./input') as fp:
     for line in fp:
-        # new passport if we encounter a new line
+        # new group if we encounter a new line
         if line == "\n" or line == "" or not line or line == None:
             answers.append(tmp_answers)
             tmp_answers = ""
         else:
-            # answers are on multiple lines, here we combine them into a
+            # groups answers are on multiple lines, here we combine them into a
             # single string that is space delimited
             tmp_answers += line.rstrip() + " "
 
@@ -19,10 +19,10 @@ sum = 0
 for answer_set in answers:
     # get the number of individuals in a group
     number_of_people = answer_set.count(" ")
-    
+
     # split space delimited answer string into a list
     answer_list = answer_set.split(" ")
-    
+
     # for each answer set (group) break it into individual's answers
     # for exactly one individual's answer, count each character across the whole list
     # compare
@@ -40,7 +40,7 @@ for answer_set in answers:
                 count += 1
         # we can break after the first iteration
         break
-    
+
     # add everyone answered to total sum
     sum += count
 
